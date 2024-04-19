d58f2f6a3536d98d261b80add0f38b39bd7597f3
Ivan_HG <ivan.desarrollo@licenciaapi.com>
2024-04-19 11:30:07 -0600

    Creacion del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+import Webcam from 'react-webcam';
+import Tesseract from 'tesseract.js';
 
-function App() {
+const App = () => {
+  const webcamRef = useRef(null);
+  const [imageData, setImageData] = useState(null);
+  const [extractedText, setExtractedText] = useState('');
+
+  const capture = () => {
+    const image = webcamRef.getScreenshot();
+
+    setImageData(image);
+  };
+
+  const recognizeText = async () => {
+    if (!imageData) return;
+
+    const tesseract = new Tesseract();
+    await tesseract.loadLanguage('spa'); // Cargar idioma español
+
+    const { data } = await tesseract.recognize(imageData);
+    setExtractedText(data.text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Webcam ref={webcamRef} />
+      <button onClick={capture}>Capturar imagen</button>
+      {
+        imageData && 
+          <img src={imageData} alt="Imagen capturada" />
+      }
+       <button onClick={recognizeText}>Extraer texto</button>
+      {extractedText && <p>Texto extraído: {extractedText}</p>}
     </div>
   );
-}
+};
 
 export default App;
